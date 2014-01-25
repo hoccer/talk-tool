555f7aa02512ce0dc79f21a2599197cf6b3beb8c
Andreas Marr <andreasm@artcom.de>
2014-01-25 15:29:07 +0100

    formatting, cleanup & slightly better reporting.

--- src/main/java/com/hoccer/talk/tool/command/ClientStatus.java
+++ src/main/java/com/hoccer/talk/tool/command/ClientStatus.java
@@ -2,12 +2,7 @@ package com.hoccer.talk.tool.command;
 
 import better.cli.annotations.CLICommand;
 import com.hoccer.talk.client.model.TalkClientContact;
-import com.hoccer.talk.model.TalkGroup;
-import com.hoccer.talk.model.TalkGroupMember;
-import com.hoccer.talk.model.TalkKey;
-import com.hoccer.talk.model.TalkPresence;
-import com.hoccer.talk.model.TalkPrivateKey;
-import com.hoccer.talk.model.TalkRelationship;
+import com.hoccer.talk.model.*;
 import com.hoccer.talk.tool.TalkToolContext;
 import com.hoccer.talk.tool.client.TalkToolClient;
 import com.hoccer.talk.tool.client.TalkToolClientCommand;
@@ -18,83 +13,95 @@ import java.util.List;
 @CLICommand(name = "cstat", description = "Show status of selected clients")
 public class ClientStatus extends TalkToolClientCommand {
 
+    public void printInd(String msg, int indentation) {
+        String defaultIndentation = "            "; // support indentation level 6 at max
+        int indentationSize = 2;
+        String ind = defaultIndentation.substring(0, indentation * indentationSize);
+        System.out.println(ind + msg);
+    }
+
     @Override
     public void runOnClient(TalkToolContext context, TalkToolClient client) {
-        System.out.println("client #" + client.getId());
 
         String clientId = client.getClientId();
-        if(clientId != null) {
-            System.out.println("  client id " + clientId);
+        if (clientId != null) {
+            printInd("client id: '" + clientId + "'", 1);
+        } else {
+            printInd("cliend id: not yet generated - register first!", 1);
         }
 
-        System.out.println("  state " + client.getClient().getStateString());
+        printInd("state: '" + client.getClient().getStateString() + "'", 1);
 
         try {
             List<TalkClientContact> contacts = client.getDatabase().findAllContacts();
-            if(contacts.isEmpty()) {
-                System.out.println("  no contacts");
+            if (contacts.isEmpty()) {
+                printInd("no contacts", 1);
             } else {
-                System.out.println("  " + contacts.size() + " contacts");
+                if (contacts.size() == 1) {
+                    printInd(contacts.size() + " contact", 1);
+                } else {
+                    printInd(contacts.size() + " contacts", 1);
+                }
             }
-            for(TalkClientContact contact: contacts) {
-                if(contact.isSelf()) {
-                    System.out.println("  contact " + contact.getClientContactId() + ": self " + contact.getClientId());
+            for (TalkClientContact contact : contacts) {
+                if (contact.isSelf()) {
+                    printInd("contact " + contact.getClientContactId() + ": (self) " + contact.getClientId(), 1);
                     TalkPresence presence = contact.getClientPresence();
-                    if(presence != null) {
-                        System.out.println("    name \"" + presence.getClientName() + "\""
-                                            + " status \"" + presence.getClientStatus() + "\"");
+                    if (presence != null) {
+                        printInd("name '" + presence.getClientName() + "'"
+                                + " status '" + presence.getClientStatus() + "'", 2);
                     } else {
-                        System.out.println("    no presence");
+                        printInd("no presence", 2);
                     }
                     TalkPrivateKey privKey = contact.getPrivateKey();
-                    if(privKey != null) {
-                        System.out.println("    private key with id " + privKey.getKeyId());
+                    if (privKey != null) {
+                        printInd("private key with id " + privKey.getKeyId(), 2);
                     }
                     TalkKey pubKey = contact.getPublicKey();
-                    if(pubKey != null) {
-                        System.out.println("    public key with id " + pubKey.getKeyId());
+                    if (pubKey != null) {
+                        printInd("public key with id " + pubKey.getKeyId(), 2);
                     } else {
-                        System.out.println("    no public key");
+                        printInd("no public key", 2);
                     }
                 }
-                if(contact.isClient()) {
-                    System.out.println("  contact " + contact.getClientContactId() + ": client " + contact.getClientId());
+                if (contact.isClient()) {
+                    printInd("contact " + contact.getClientContactId() + ": (client) " + contact.getClientId(), 1);
                     TalkRelationship relationship = contact.getClientRelationship();
-                    if(relationship != null) {
-                        System.out.println("    relationship " + relationship.getState());
+                    if (relationship != null) {
+                        printInd("relationship " + relationship.getState(), 2);
                     } else {
-                        System.out.println("    no relationship");
+                        printInd("no relationship", 2);
                     }
                     TalkPresence presence = contact.getClientPresence();
-                    if(presence != null) {
-                        System.out.println("    name \"" + presence.getClientName() + "\""
-                                            + " status \"" + presence.getClientStatus() + "\""
-                                            + " connState " + presence.getConnectionStatus());
+                    if (presence != null) {
+                        printInd("name '" + presence.getClientName() + "'"
+                                + " status '" + presence.getClientStatus() + "'"
+                                + " connState " + presence.getConnectionStatus(), 2);
                     } else {
-                        System.out.println("    no presence");
+                        printInd("no presence", 2);
                     }
                     TalkKey pubKey = contact.getPublicKey();
-                    if(pubKey != null) {
-                        System.out.println("    public key with id " + pubKey.getKeyId());
+                    if (pubKey != null) {
+                        printInd("public key with id '" + pubKey.getKeyId() + "'", 2);
                     } else {
-                        System.out.println("    no public key");
+                        printInd("no public key", 2);
                     }
                 }
-                if(contact.isGroup()) {
-                    System.out.println("  contact " + contact.getClientContactId() + ": group " + contact.getGroupId());
+                if (contact.isGroup()) {
+                    printInd("contact " + contact.getClientContactId() + ": group " + contact.getGroupId(), 1);
                     TalkGroup group = contact.getGroupPresence();
-                    if(group != null) {
-                        System.out.println("    name \"" + group.getGroupName() + "\""
-                                            + " state " + group.getState());
+                    if (group != null) {
+                        printInd("name '" + group.getGroupName() + "'"
+                                + " state " + group.getState(), 2);
                     } else {
-                        System.out.println("    no presence");
+                        printInd("no presence", 2);
                     }
                     TalkGroupMember member = contact.getGroupMember();
-                    if(member != null) {
-                        System.out.println("    state " + member.getState()
-                                            + " role " + member.getRole());
+                    if (member != null) {
+                        printInd("state " + member.getState()
+                                + " role " + member.getRole(), 2);
                     } else {
-                        System.out.println("    no membership");
+                        printInd("no membership", 2);
                     }
                 }
             }
