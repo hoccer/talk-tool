5bc1b5c1fc1ab288dfe4061969a51c8f8760f02b
Andreas Marr <andreasm@artcom.de>
2014-04-09 19:01:56 +0200

    clarified cginvite command line params.

--- src/main/java/com/hoccer/talk/tool/command/ClientGroupInvite.java
+++ src/main/java/com/hoccer/talk/tool/command/ClientGroupInvite.java
@@ -12,25 +12,29 @@ import java.sql.SQLException;
 @CLICommand(name = "cginvite", description = "Invite another client to a group")
 public class ClientGroupInvite extends TalkToolClientCommand {
 
-    @Parameter(names = "-g", description = "Group to invite to", required = true)
+    @Parameter(names = "-g", description = "GroupId of group to invite to", required = true)
     String pGroup;
-    @Parameter(names = "-t", description = "Target of invitation", required = true)
+
+    @Parameter(names = "-t", description = "Target of invitation - accepts only toolclients currently", required = true)
     String pTarget;
 
     @Override
     public void runOnClient(TalkToolContext context, TalkToolClient client) throws Exception {
         TalkToolClient target = context.getClientBySelector(pTarget);
+        if (target == null) {
+            throw new Exception("Need to provide valid target");
+        }
+
         TalkClientContact group = null;
         try {
             group = client.getDatabase().findContactByGroupId(pGroup, false);
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        if (target != null && group != null) {
-            client.getClient().inviteClientToGroup(group.getGroupId(), target.getClientId());
-        } else {
+        if (group == null) {
             throw new Exception("Need to provide valid target and group");
         }
-    }
 
+        client.getClient().inviteClientToGroup(group.getGroupId(), target.getClientId());
+    }
 }
